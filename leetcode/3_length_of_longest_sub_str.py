84b606d9abaf6fde111d7cb5f3e52d3f3e867b9c
hlliu <781107080@qq.com>
2018-04-13 00:15:31 +0800

    update 3

--- leetcode/3_length_of_longest_sub_str.py
+++ leetcode/3_length_of_longest_sub_str.py
@@ -6,27 +6,56 @@
 def test_length_of_longest_sub_str():
     '''
     思路：
-    时间复杂度：
+    1.0
+    时间复杂度：O(n^2), 596ms
     '''
     def length_of_longest_sub_str(s):
         dict = {}
-        start = end = length = 0
+        length = 0
+        # start = end = 0
+
         for i in range(len(s)):
             if s[i] in dict.keys():
                 index = dict.get(s[i])
-                start1 = min(dict.values())
-                end1 = max(dict.values())
-                if end1 - start1+1 > length:
-                    length = end1 - start1 +1
-                    start = start1
-                    end = end1
+                start_temp = min(dict.values())
+                end_temp = max(dict.values())
+                if end_temp - start_temp + 1 > length:
+                    length = end_temp - start_temp + 1
+                    # start = start_temp
+                    # end = end_temp
+
+                # 添加新的非重复的元素
                 dict = {}
                 for j in range(index+1, i+1):
                     dict[s[j]] = j
             else:
                 dict[s[i]] = i
 
-        return s[start:end]
+        if len(dict.items()) > length:
+            length = len(dict.items())
+            # start = min(dict.values())
+            # end = max(dict.values())
+
+        # print(s[start:end+1])
+        return length
+
+    '''
+    思路：
+    1.0
+    时间复杂度：O(n), 152ms
+    '''
+    def length_of_longest_sub_str2(s):
+        dict = {}
+        length = index = 0
+
+        for i in range(len(s)):
+            if s[i] in dict.keys():
+                index = max(index, dict.get(s[i]) + 1)
+
+            dict[s[i]] = i
+            length = max(length, i - index + 1)
+
+        return length
 
     print()
-    print(length_of_longest_sub_str('abcabcbb'))
+    print(length_of_longest_sub_str2('bbbb'))
