53a86ccf3a6c70bdf6bc4f856d974675354877e5
liuhonglian [刘红莲] <liuhonglian@xiaomi.com>
2018-04-13 11:22:05 +0800

    add add 3

--- leetcode/3_length_of_longest_sub_str.py
+++ leetcode/3_length_of_longest_sub_str.py
@@ -6,10 +6,15 @@
 def test_length_of_longest_sub_str():
     '''
     思路：
-    1.0
+    1.遍历
+    2.存dict（值，下标）
+    3.遇到重复
+        3.1 取得第一个值下标
+        3.2 比较赋值最长子串
+        3.3 截取新的计算的子串（即第一个重复下标+1 开始）
     时间复杂度：O(n^2), 596ms
     '''
-    def length_of_longest_sub_str(s):
+    def length_of_longest_sub_str1(s):
         dict = {}
         length = 0
         # start = end = 0
@@ -41,13 +46,19 @@ def test_length_of_longest_sub_str():
 
     '''
     思路：
-    1.0
+    1.遍历
+    2.存dict（值，下标）
+    3.遇到重复
+        3.1 获取最新比较子串的起始下标
+    4.计算最长的子串
     时间复杂度：O(n), 152ms
     '''
     def length_of_longest_sub_str2(s):
         dict = {}
+        # index 为新子串的起始下标
         length = index = 0
 
+        # 每次都计算一次length
         for i in range(len(s)):
             if s[i] in dict.keys():
                 index = max(index, dict.get(s[i]) + 1)
@@ -57,5 +68,31 @@ def test_length_of_longest_sub_str():
 
         return length
 
+
+    '''
+    思路：
+    1.遍历
+    2.存dict（值，下标）
+    3.遇到重复
+        3.1 获取最新比较子串的起始下标
+    4.计算最长的子串
+    时间复杂度：O(n), 92 ms
+    '''
+    def length_of_longest_sub_str3(s):
+        dict = {}
+        # index 为新子串的起始下标
+        length = index = 0
+
+        # 当index无变化时计算length
+        for i in range(len(s)):
+            if s[i] in dict.keys() and index <= dict.get(s[i]):
+                index = dict.get(s[i]) + 1
+            else:
+                length = max(length, i - index + 1)
+
+            dict[s[i]] = i
+
+        return length
+
     print()
-    print(length_of_longest_sub_str2('bbbb'))
+    print(length_of_longest_sub_str3('tmmzuxt'))
