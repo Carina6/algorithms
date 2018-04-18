56de71660835b6a6aab8285304d2f55cb23d6924
hlliu <781107080@qq.com>
2018-04-19 00:24:57 +0800

    update 4

--- leetcode/4_median_of_two_sorted_array.py
+++ leetcode/4_median_of_two_sorted_array.py
@@ -10,24 +10,62 @@ def test_find_median_sorted_arrays():
     '''
     def find_median_sorted_arrays(nums1, nums2):
         median_len = (len(nums1) + len(nums2)) // 2
-        min = max = 0
+        carry = (len(nums1) + len(nums2)) % 2
 
         i1 = i2 = 0
-        j1 = len(nums1) - 1
-        j2 = len(nums2) - 1
-        for i in range(median_len):
+        arr = []
+        for i in range(median_len + 1):
 
-            if nums1[i1] <= nums2[i2] and i1 < j1:
+            if i1 < len(nums1) and i2 < len(nums2):
+                if nums1[i1] < nums2[i2]:
+                    arr.append(nums1[i1])
+                    i1 += 1
+                else:
+                    arr.append(nums2[i2])
+                    i2 += 1
+            elif i1 == len(nums1) and i2 < len(nums2):
+                arr.append(nums2[i2])
+                i2 += 1
+            elif i2 == len(nums2) and i1 < len(nums1):
+                arr.append(nums1[i1])
                 i1 += 1
             else:
-                i2 += 1
+                break
 
-            if nums1[j1] <= nums2[j2] and j2 > i2:
-                j2 -= 1
+        if len(arr) == 0:
+            return 0
+
+        if carry > 0:
+            return arr[median_len]
+        else:
+            return (arr[median_len-1] + arr[median_len]) / 2
+
+    def find_median_sorted_arrays2(nums1, nums2):
+        A, B = nums1, nums2
+        m = len(A)
+        n = len(B)
+        if m > n:
+            A, B, m, n = B, A, n, m
+
+        k = m+n+1
+        i = m+1
+        j = k - i
+
+        while True:
+            l1 = (i-1)//2
+            r1 = i//2
+            l2 = (j-1)//2
+            r2 = j//2
+
+            if A[l1] > B[r2]:
+                i = i//2
+                j = k-i
+            if B[l2] > A[r1]:
+                i += 1
+                j = k-i
             else:
-                j1 -= 1
-
+                return (max(A[l1], B[l2]) + min(A[r1], B[r2])) / 2
 
 
     print()
-    print()
+    print(find_median_sorted_arrays([1], [0]))
