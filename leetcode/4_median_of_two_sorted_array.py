528f07bae957c43d28626d106dacf42eb56ad99c
hlliu <781107080@qq.com>
2018-04-20 00:19:46 +0800

    update 4

--- leetcode/4_median_of_two_sorted_array.py
+++ leetcode/4_median_of_two_sorted_array.py
@@ -43,38 +43,60 @@ def test_find_median_sorted_arrays():
         else:
             return (arr[median_len-1] + arr[median_len]) / 2
 
+    '''
+    思路：创建(m+n)/2 + 1长度的数组arr，存前(m+n)/2 + 1 个最小数，
+         如果两数组长度和为奇数，则中位数为arr中的最后一位，为偶数，则为最后两位的平均数
+    1.遍历nums1,nums2
+    2.选取最小的存在arr
+    时间复杂度：O(log(min(m, n))) 108 ms
+    '''
     def find_median_sorted_arrays2(nums1, nums2):
-        A, B = nums1, nums2
-        m = len(A)
-        n = len(B)
+        m = len(nums1)
+        n = len(nums2)
+        # 二分长度较小的数组，使时间复杂度更小
         if m > n:
-            A, B, m, n = B, A, n, m
+            nums1, nums2, m, n = nums2, nums1, n, m
 
-        imin, imax = 0, m-1
+        # 二分法的初始最小，最大值
+        imin, imax = 0, m
+        # 两个数组的中位数的位置
         k = (m + n + 1) // 2
+        # 两数组长度是奇数还是偶数
         carry = (m + n) % 2
 
+        # 开始二分算法
         while imin <= imax:
-            c1 = imin + (imin + imax) // 2
+            c1 = (imin + imax) // 2
             c2 = k-c1
 
-            if c1 > 0 and A[c1-1] > B[c2]:
+            if c1 > 0 and nums1[c1-1] > nums2[c2]:
+                # 分割点太大，减少
                 imax = c1-1
-            elif c2 > 0 and B[c2-1] > A[c1]:
+            elif c1 < m and nums2[c2-1] > nums1[c1]:
+                # 分割点太小， 增加
                 imin = c1+1
             else:
-                lmax = 0
-                if c1 > 0 and c2 > 0:
-                    lmax = max(A[c1 - 1], B[c2 - 1])
-                elif c1 > 0:
-                    lmax = A[c1 - 1]
-                elif c2 > 0:
-                    lmax = B[c2 - 1]
-
-                if carry == 0:
-                    return (lmax + min(A[c1], B[c2])) / 2
+                # 此时找到分割点c1
+                # lmax 为两数组分割点左边的最大数
+                if c2 == 0:
+                    lmax = nums1[c1 - 1]
+                elif c1 == 0:
+                    lmax = nums2[c2 - 1]
                 else:
+                    lmax = max(nums1[c1 - 1], nums2[c2 - 1])
+
+                if carry == 1:
                     return lmax
 
+                # rmin 为两数组分割点右边的最小值
+                if c1 == m:
+                    rmin = nums2[c2]
+                elif c2 == n:
+                    rmin = nums1[c1]
+                else:
+                    rmin = min(nums1[c1], nums2[c2])
+
+                return (lmax + rmin) / 2
+
     print()
-    print(find_median_sorted_arrays2([1,3], [2]))
+    print(find_median_sorted_arrays2([2], [1]))
