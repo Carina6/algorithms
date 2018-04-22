038f3db75c8a8d4ed338713d185e13215240624a
hlliu <781107080@qq.com>
2018-04-22 22:30:21 +0800

    update 6

--- leetcode/6_zigzag_conversion.py
+++ leetcode/6_zigzag_conversion.py
@@ -7,31 +7,86 @@ import numpy
 def test_zigzag_conversion():
     '''
     思路：
-    时间复杂度：O(n) 140 ms
+    时间复杂度：O(n) 168 ms
     '''
     def zigzag_conversion(s, numRows):
         res = []
         if numRows < 2:
             return s
 
-        column = len(s) // (2 * numRows-2)
-        if len(s) % (2*numRows-2) > 0:
+        # 二维数组的行数，numRows + numRows -2
+        row = 2 * numRows-2
+        # 二维数组列数
+        column = len(s) // row
+        if len(s) % row > 0:
             column += 1
-        # N1 = numpy.zeros([column, numRows])
-        N1 = [[None for x in range(2 * numRows-2)] for y in range(column)]
+
+        # 初始化二维数组，row x column
+        N = [[None for x in range(column)] for y in range(row)]
+
         for i in range(len(s)):
-            row_i = i // (2*numRows-2)
-            column_i = i % (2*numRows-2)
-            N1[row_i][column_i] = s[i]
+            column_i = i // row
+            row_i = i % row
+            N[row_i][column_i] = s[i]
 
         for j in range(numRows):
             for k in range(column):
-                if N1[k][j]:
-                    res.append(N1[k][j])
-                if j % numRows not in [0, numRows-1] and N1[k][2*numRows-2-j]:
-                    res.append(N1[k][2*numRows-2-j])
+                if N[j][k]:
+                    res.append(N[j][k])
+                if j % numRows not in [0, numRows-1] and N[row-j][k]:
+                    res.append(N[row-j][k])
 
         return ''.join(res)
 
+    '''
+    思路：使用numRows个数组存字符串值
+    时间复杂度：O(n) 108 ms
+    '''
+    def zigzag_conversion2(s, numRows):
+        if numRows < 2:
+            return s
+
+        L = [[]]*numRows
+        j = 0
+        while j < len(s):
+            for k in range(numRows):
+                if j < len(s):
+                    L[k].append(s[j])
+                    j += 1
+            for h in range(numRows-2, 0, -1):
+                if j < len(s):
+                    L[h].append(s[j])
+                    j += 1
+        res = ''
+        for i in range(numRows):
+            res += ''.join(L[i])
+
+        return res
+
+    '''
+    思路：使用numRows个字符串，按照Z字形存储字符
+    时间复杂度：O(n) 80 ms
+    '''
+    def zigzag_conversion3(s, numRows):
+        if numRows < 2:
+            return s
+
+        # 定义numRows个数组
+        L = ['']*numRows
+
+        index, step = 0, 1
+        for x in s:
+            L[index] += x
+            # 当Z字形向下时，step = 1
+            if index == 0:
+                step = 1
+            # 当Z字形向上时，step = -1
+            elif index == numRows-1:
+                step = -1
+            index += step
+
+        return ''.join(L)
+
+
     print()
-    print(zigzag_conversion('ABCDE', 4))
+    print(zigzag_conversion3('PAYPALISHIRING', 3))
