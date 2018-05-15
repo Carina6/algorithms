ba8cf33429a9aa2792565edf8f8323c638cc7130
hlliu <781107080@qq.com>
2018-05-16 00:09:06 +0800

    update 17

--- leetcode/17_letter_combinations.py
+++ leetcode/17_letter_combinations.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from itertools import product
 
 
 # 17. Letter Combinations of a Phone Number
@@ -8,8 +9,29 @@ def test_letter_combinations():
     思路：
     时间复杂度：
     '''
-    def letter_combinations():
-        pass
+    def letter_combinations(digits):
+        res = []
+        if not digits:
+            return res
+
+        c = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+
+        temp = list(c[int(digits[0])])
+        for i in range(1, len(digits)):
+            if c[int(digits[i])] == '':
+                continue
+            elif len(temp) == 0:
+                temp = c[int(digits[i])]
+            else:
+                temp = list(product(temp, c[int(digits[i])]))
+
+            if len(temp[0]) == 2:
+                res = []
+                for j in range(len(temp)):
+                    res.append(temp[j][0]+temp[j][1])
+                temp = res
+
+        return temp
 
     print()
-    print()
+    print(letter_combinations('2'))
