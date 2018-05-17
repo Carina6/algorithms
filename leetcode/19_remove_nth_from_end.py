9b689b46c030636f0e1d54fc924bfc4a0bc7f811
hlliu <781107080@qq.com>
2018-05-17 23:40:23 +0800

    update 19

--- leetcode/19_remove_nth_from_end.py
+++ leetcode/19_remove_nth_from_end.py
@@ -19,16 +19,35 @@ def test_remove_nth_from_end():
             t_head_len = t_head_len.next
 
         i = 0
-        while i < length - n - 1:
-            t_head = t_head.next
-            i += 1
-        if t_head == head:
+        if n == length:
             head = head.next
         else:
+            while i < length - n - 1:
+                t_head = t_head.next
+                i += 1
+
             t_head.next = t_head.next.next
+        return head
+
+    def remove_nth_from_end2(head, n):
+        if head is None:
+            return head
+
+        f_head, s_head = head, head
+        for i in range(n):
+            f_head = f_head.next
+
+        if not f_head:
+            return head.next
+
+        while f_head.next:
+            s_head = s_head.next
+            f_head = f_head.next
+        s_head.next = s_head.next.next
+
         return head
 
     print()
     head = list_node_generator([1, 2])
     print(head)
-    print(remove_nth_from_end(head, 1))
+    print(remove_nth_from_end2(head, 1))
