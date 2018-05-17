39b48f944cae71a2f547c0d66b03537bfb8a3e09
liuhonglian [刘红莲] <liuhonglian@xiaomi.com>
2018-05-17 18:26:50 +0800

    update 19

---
+++ leetcode/19_remove_nth_from_end.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# 19. Remove Nth Node From End of List
+def test_remove_nth_from_end():
+    '''
+    思路：
+    时间复杂度：
+    '''
+    def remove_nth_from_end(head, n):
+        pass
+
+    print()
+    print(remove_nth_from_end())
