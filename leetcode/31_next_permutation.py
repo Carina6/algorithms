01771581794f8f43c2adfbbb018210ff9286ca07
刘红莲 <liuhonglian@xiaomi.com>
2018-07-11 17:10:19 +0800

    update 30

--- leetcode/31_next_permutation.py
+++ leetcode/31_next_permutation.py
@@ -11,20 +11,28 @@ def test_next_permutation():
 
     def next_permutation(nums):
         index = 0
-        j = len(nums) - 1
+        nums_len = len(nums)
         for i in range(len(nums) - 1, 0, -1):
-            if nums[i] > nums[i - 1]:
-                nums[i - 1], nums[j] = nums[j], nums[i - 1]
-                temp = nums[i-1:j + 1]
+            # 从最右边开始遍历，选取第一个小于后一位的数字，记录下标i-1
+            if nums[i - 1] < nums[i]:
+                # 截取i-1后的数组，并排序
+                temp = nums[i - 1:nums_len]
                 temp.sort()
                 k = 0
                 h = i
-                while h <= j:
-                    if temp[k] > nums[i]:
-
-                    nums[h] = temp[k]
-                    h += 1
+                flag = True  # flag表示是否找到截取数组中大于nums[i-1]的最小值
+                while h <= nums_len-1:
+                    # 因为temp 数组已经排序，所以第一个大于nums[i]的数即为截取数组中大于nums[i-1]的最小值
+                    if flag and temp[k] > nums[i - 1]:
+                        nums[i - 1] = temp[k]
+                        flag = False
+                    else:
+                        # 从小标i开始依次填充数值
+                        nums[h] = temp[k]
+                        h += 1
                     k += 1
+                if k < len(temp):
+                    nums[i - 1] = temp[k]
                 break
             index = i
         if index == 1:
@@ -32,4 +40,4 @@ def test_next_permutation():
         return nums
 
     print()
-    print(next_permutation([2, 3, 1]))
+    print(next_permutation([5, 4, 7, 5, 3, 2]))
