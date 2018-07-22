256be0a95b55d85e406fc31ae3e8b9e8940244a6
hlliu <781107080@qq.com>
2018-07-22 23:49:05 +0800

    update 33

--- leetcode/33_search_in_rotated_sorted_arr.py
+++ leetcode/33_search_in_rotated_sorted_arr.py
@@ -19,10 +19,27 @@ def test_search():
     # 二分法
     def search2(nums, target):
         l = len(nums)
-        if l ==0:
+        if l == 0:
             return -1
-
-        pass
+        start, end = 0, l - 1
+        while start < end:
+            mid = (start + end) // 2
+            if nums[mid] > nums[end]:
+                start = mid + 1
+            else:
+                end = mid
+        rot = start
+        start, end = 0, l - 1
+        while start <= end:
+            mid = (start + end) // 2
+            relmid = (mid + rot) % l
+            if nums[relmid] < target:
+                start = mid + 1
+            elif nums[relmid] > target:
+                end = mid - 1
+            else:
+                return relmid
+        return -1
 
     print()
-    print(search([4,5,6,7,0,1,2], 0))
+    print(search2([4, 5, 6, 7, 0, 1, 2], 0))
