99dbd07e9622c6ab9d6420ec81c9764c5ca8dbbe
hlliu <781107080@qq.com>
2019-03-28 00:33:47 +0800

    update

--- models/generator.py
+++ models/generator.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from queue import Queue
+
+from models.binary_tree import Node
 from models.list_node import ListNode
 
 
@@ -12,3 +15,22 @@ def list_node_generator(nums):
         t.next = ListNode(nums[i])
         t = t.next
     return head
+
+
+def tree_node_generator(nums):
+    if len(nums) == 0:
+        return None
+    root = Node(nums[0])
+    q = Queue()
+    q.put(root)
+    for i in range(1,len(nums),2):
+        tmp = q.get()
+        tmp.left = Node(nums[i])
+        q.put(tmp.left)
+        try:
+            tmp.right = Node(nums[i + 1])
+            q.put(tmp.right)
+        except IndexError:
+            pass
+
+    return root
--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2018/10/23 下午11:41
 # @Author  : hlliu
+from models.generator import tree_node_generator
 
 
 def test():
@@ -32,3 +33,15 @@ def test():
 def test2():
     map = dict()
     print(map.get("a"))
+
+
+def test3():
+    arr = [1,2,3,4,5,6]
+    for i in arr[1::2]:
+        print(i)
+
+
+def test4():
+    arr = [1,2,3,4,5,6]
+    node = tree_node_generator(arr)
+    print(node)
