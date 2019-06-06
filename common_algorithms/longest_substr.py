bd5dcb874096da38e841391dc73360b0941553ce
刘红莲 <liuhonglian@xiaomi.com>
2019-06-06 11:23:10 +0800

    refactor

--- common_algorithms/longest_substr.py
+++ common_algorithms/longest_substr.py
@@ -6,20 +6,19 @@
 
 # 最大连续子序列
 def test_longest_substr():
-
     def method1(str):
         if str is None or len(str) == 0:
             return 0
         index = max_count = 0
-        tmp = 1
-        for i in range(len(str)-1):
-            if str[i] == str[i+1]:
-                tmp+=1
+        count_tmp = 1
+        for i in range(len(str) - 1):
+            if str[i] == str[i + 1]:
+                count_tmp += 1
             else:
-                if max_count<tmp:
-                    max_count = tmp
-                    index = i-max_count+1
-                tmp = 1
-        return str[index:index+max_count]
+                if max_count < count_tmp:
+                    max_count = count_tmp
+                    index = i - max_count + 1
+                count_tmp = 1
+        return str[index:index + max_count]
 
     print(method1('amcdddddlaeeefn'))
